330a45d272a4229b15cafc72ea2f9b4a51f62a9d
snailone <snail1@qq.com>
2015-04-14 18:30:51 +0100

    update

--- scripts/menuPage.js
+++ scripts/menuPage.js
@@ -1,4 +1,4 @@
-$(document).on("pagebeforeshow", "#menuPage", function() {
+$(document).on("pageinit", "#menuPage", function() {
 	$("#showMenu").text("");
 	$("#showType").text("");
 	$.getJSON("includes/readmenu.php", function(data){
@@ -24,9 +24,16 @@ $(document).on("pagebeforeshow", "#menuPage", function() {
         	$("#showType").append("<li><a class='ui-btn' id='dishType" + key + "'>" + value + "</a></li>");
         	$("#dishType" + key).click(function() {
         		$("#showMenu").text("");
+        		var i = 0;
         		$.each(allDishType, function(key1, value1) {
         			if (value1 == value) {
-        				addMenuDish(allDishID[key1], allDishName[key1], allPrice[key1]);
+        				if (i%2 == 0) {
+        					$('#showMenu').append("<div class='ui-grid-a'></div>");
+        					addMenuDish(allDishID[key1], allDishName[key1], allPrice[key1], "left");
+        				} else {
+        					addMenuDish(allDishID[key1], allDishName[key1], allPrice[key1], "right");
+        				}
+        				i = i + 1;
         			}
         		});
         	});
@@ -80,8 +87,8 @@ $(document).on("pagebeforeshow", "#menuPage", function() {
         });
 });*/
 
-function addMenuDish(id, name, price) {
-	$("#showMenu").append(listMenu(id, name, price));
+function addMenuDish(id, name, price, position) {
+	$('#showMenu').children().last().append(listMenu(id, name, price, position));
 	dishState(id);
 	$("#dish" + id).click(function() {
 		storeObject.DishID = id;
@@ -94,23 +101,23 @@ function addMenuDish(id, name, price) {
 	});
 }
 
-function listMenu(id, name, price) {
-	var list = "<li class='ui-li-has-alt' id='dishBlock" + id + "'>"
-			 +   "<div class='ui-grid-a my-breakpoint'>"
-			 +     "<div class='ui-block-a'>"
-			 +       "<a id='dish" + id + "' href='#singlePage' class='ui-btn ui-mini' style='text-align:left; margin:0 0 0 0;' >"
-			 +         "<h2>"+ name + "</h2>"
-			 +         "<p>Price: &pound;" + price + " <span style='color:red' id='dishNum" + id + "'></span></p>"
-			 +       "</a>"
+function listMenu(id, name, price, position) {
+	var list;
+	if (position == "left") {
+		list = "<div class='ui-block-a' id='dishBlock" + id + "'>";
+	} else {
+		list = "<div class='ui-block-b' id='dishBlock" + id + "'>";
+	}
+	list +=    "<a id='dish" + id + "' href='#singlePage' class='ui-btn ui-mini' style='text-align:left; margin:0 0 0 0;' >"
+			 +       "<img src='images/menu/dish"+id+".jpg' alt='dish' style='max-width:100%'>"
+			 +       "<h2>"+ name + "</h2>"
+			 +       "<p>Price: &pound;" + price + " <span style='color:red' id='dishNum" + id + "'></span></p>"
+			 +     "</a>"
+			 +     "<div style='text-align:right'>"
+			 +       "<a id='dishMinus" + id + "' href='#' class='ui-btn ui-mini ui-btn-inline ui-btn-icon-notext ui-icon-minus ui-corner-all'>Minus</a>"
+			 +       "<a id='dishPlus" + id + "' href='#' class='ui-btn ui-mini ui-btn-inline ui-btn-icon-notext ui-icon-plus ui-corner-all'>Plus</a>"
 			 +     "</div>"
-			 +     "<div class='ui-block-b' style='height:100%'>"
-			 +       "<div style='float:right;'>"
-			 +         "<a id='dishMinus" + id + "' href='#' class='ui-btn ui-mini ui-btn-inline ui-btn-icon-notext ui-icon-minus ui-corner-all'>Minus</a>"
-			 +         "<a id='dishPlus" + id + "' href='#' class='ui-btn ui-mini ui-btn-inline ui-btn-icon-notext ui-icon-plus ui-corner-all'>Plus</a>"
-			 +       "</div>"
-			 +     "</div>"
-			 +   "</div>"
-			 + "</li>";
+			 +   "</div>";
 	return list;
 }
 

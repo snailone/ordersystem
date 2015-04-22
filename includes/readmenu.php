8e01a07747eff9363daebf3bd7a566a0c3fc0cc7
snailone <snail1@qq.com>
2015-04-22 14:13:24 +0100

    update

--- includes/readmenu.php
+++ includes/readmenu.php
@@ -15,16 +15,16 @@ if ($result->num_rows > 0) {
 $sql = "select DishID, avg(Star) as Star, count(*) as Num from dishcomment group by DishID";
 $result = $conn->query($sql);
 
-if ($result->num_rows > 0) {
-	while($row = $result->fetch_assoc()) {
-		$star[$row["DishID"]] = $row;
-	}
-} else {
-	echo "0 results";
+while($row = $result->fetch_assoc()) {
+	$star[$row["DishID"]] = $row;
 }
 
 foreach ($outp as $id => $value) {
-	$outp[$id] = array_merge($value, $star[$id]);
+	if ( isset($star[$id]) ) {
+		$outp[$id] = array_merge($value, $star[$id]);
+	} else {
+		$outp[$id] = array_merge($value, array("Star"=>"0", "Num"=>"0"));
+	}
 }
 
 $conn->close();

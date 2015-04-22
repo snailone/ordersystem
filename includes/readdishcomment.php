dd55a4c8bfefae03d98f42064c4205e06e5b772e
snailone <snail1@qq.com>
2015-04-22 14:18:06 +0100

    update

--- includes/readdishcomment.php
+++ includes/readdishcomment.php
@@ -5,6 +5,8 @@ $dishid = $_POST["DishID"];
 $sql = "select DishID, Time, Comment from dishcomment where DishID=" . $dishid;
 $result = $conn->query($sql);
 
+$outp = "";
+
 if ($result->num_rows > 0) {
 	$outp = "[";
 	while($row = $result->fetch_assoc()) {
